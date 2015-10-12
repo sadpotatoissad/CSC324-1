3cb22d05b38bebb4ac2949630c481b0b40f74006
Song Jiankun <g4songji@cdf.toronto.edu>
2015-10-12 19:12:39 -0400

    finished general a1

--- a1/mya1.rkt
+++ a1/mya1.rkt
@@ -129,15 +129,23 @@ Read through the starter code carefully. In particular, look for:
   ("a" "charming" "young" "nobleman" "from" "Verona"))
 
 |#
-
-
 (define dialogue "dialogue")
-(define settingsFuncNames '())
+
+;'allCharactersNames' and 'allCharactersValues' are two lists of names and values from Persona
 (define allCharactersName '())   
 (define allCharactersValues '())
-(define outputDialogueNames '())
 
-(define arithmeticNames (list add mult))
+;settingsFuncNames and settingsFuncString are two lists of names and string from Settings
+;settingsFuncStatus represents the mode of each function where
+; 0 - takes 0 arguements and calls no other functions
+; 1 - takes 1 arguements and calls no other functions
+; 2 - takes 1 arguements and calls another function
+; 3 - takes 0 arguements and calls another function
+(define settingsFuncNames '())
+(define settigsFuncStatus '())
+(define settingsFuncString '())
+
+(define outputDialogueNames '())
 
 ;splits text into three bodies: personae, Settings and dialogue
 (define (bodyParser body)
@@ -147,20 +155,28 @@ Read through the starter code carefully. In particular, look for:
     (if (string=? settings (first (list-tail body (+ (length dPersonae) 2))))
         (rest (reverse (list-tail (reverse settingBody) (length (member finis settingBody)))))
         '()))
-  (define dialogue '())
+  (define dialogueList '())
   (if (eq? 0 (length set))
-      (set! dialogue settingBody)
-      (set! dialogue (list-tail settingBody (+ (length set) 2))))
-  (list dPersonae set dialogue))
+      (set! dialogueList settingBody)
+      (set! dialogueList (list-tail settingBody (+ (length set) 2))))
+  (list dPersonae set dialogueList))
 
+;returns true of given list is a sublist of another list
+(define (sublistHelper sub lst)
+  (if (empty? sub)
+      #t
+      (if (empty? lst)
+          #f
+          (if (string=? (first sub) (first lst))
+              (sublistHelper (rest sub) (rest lst))
+              #f))))
 
-;evaluates the value of a given line
-
+;finds the index of a the sublist in a list or else returns false
 (define (sublist sub lst)
   (if (eq? sub '())
       0
       (if (member (first sub) lst)
-          (if (checkRest sub (member (first sub) lst))
+          (if (sublistHelper sub (member (first sub) lst))
               (- (length lst) (length (member (first sub) lst)))
               (if (eq? (sublist sub (rest(member (first sub) lst))) #f)
                   #f
@@ -169,33 +185,14 @@ Read through the starter code carefully. In particular, look for:
                       1)))
           #f)))
 
-(define (checkRest sub lst)
-  (if (empty? sub)
-      #t
-      (if (empty? lst)
-          #f
-          (if (string=? (first sub) (first lst))
-              (checkRest (rest sub) (rest lst))
-              #f))))
+;counts the number of occurences of a keyword list in a given line
+(define (sublistEvaluation keywordsList line)
+  (if (eq? empty keywordsList)
+      0
+      (if (eq? 0 (sublist (string-split (first keywordsList)) line))
+          (+ 1 (sublistEvaluation (rest keywordsList) line))
+          (sublistEvaluation (rest keywordsList) line))))
 
-
-(define (sublistEvaluation mode keywordsList line)
-  (cond
-    [(string=? "includeFuncCall" mode)
-     (if (eq? empty keywordsList)
-         0
-         (if (eq? 0 (sublist (string-split (first keywordsList)) line))
-             (+ 1 (sublistEvaluation mode (rest keywordsList) line))
-             (sublistEvaluation mode (rest keywordsList) line)))]
-;    [(string=? "arithmeticLocation" mode)
- ;    (pretty-print line)
-  ;    (if (eq? empty keywordsList)
-   ;      '()
-    ;     (cons (sublist (string-split (first keywordsList)) line) (sublistEvaluation mode (rest keywordsList) line)))]
-    [(string=? dialogue mode)
-     0]))
-
- 
 ;evaluates the number of keywords in given a specific mode and a string
 (define (countKeyWords mode line)
   (cond
@@ -210,9 +207,7 @@ Read through the starter code carefully. In particular, look for:
          1
          0)]
     [(string=? "includeFuncCall" mode)
-     (sublistEvaluation mode settingsFuncNames (string-split line))]
- ;   [(string=? "arithmeticLocation" mode)
- ;    (sublistEvaluation mode arithmeticNames (string-split line))]
+     (sublistEvaluation settingsFuncNames (string-split line))]
     [(string=? dialogue mode)
      (if (eq? empty line)
          0
@@ -226,145 +221,158 @@ Read through the starter code carefully. In particular, look for:
              (+ 1 (countKeyWords mode (rest line)))
              (countKeyWords mode (rest line))))]))
 
-
+;recursively loops through names to update values
 (define (calculateValuesWithNames textLine value charactersName charactersValues)
   (cond
     [(eq? empty charactersName)
-     value
-     ]
+     value]
     [else
      (define numCharName (countKeyWords (first charactersName) textLine))
      (define newValue (+ (- value numCharName) (* numCharName (first charactersValues))))
-     (calculateValuesWithNames textLine newValue (rest charactersName) (rest charactersValues))
-     ]
-    )
-)
+     (calculateValuesWithNames textLine newValue (rest charactersName) (rest charactersValues))]))
 
-(define (calculateValues textLine counter)
+;update values using name substitution
+(define (calculateValuesArthmeticHelper textLine selfValue)
+  (define initialValue (evaluateLine personae textLine 0))
+  (if (number? (first textLine))
+      (first textLine)
+      (cond
+        [(eq? 1 (length textLine))
+         (define numOfSelf (countKeyWords dialogue textLine))
+         (define initValue (evaluateLine personae textLine 0))
+         (if (eq? 0 numOfSelf)
+             (calculateValuesWithNames textLine initValue allCharactersName allCharactersValues)
+             selfValue)]
+        [else
+         (evaluateLine personae textLine 0)])))
+
+;calculate values with arthithemtic operations
+(define (calculateValuesArithmetic textLine counter)
   (define addCut (sublist (string-split add) textLine))
   (define multCut (sublist (string-split mult) textLine))
-  
-  (define selfName (list-ref outputDialogueNames counter))
-  (define selfIndex (- (length allCharactersName) (length (member selfName allCharactersName))))
-  (define selfValue (list-ref allCharactersValues selfIndex))
- 
-  ;(pretty-print textLine)
-  ;(pretty-print (calculateValuesWithNames textLine textLine))
-  
-   (cond
+  (define selfValue 0)
+  ;augment operations based on if input is from dialogue or from settings
+  (cond
+    [(eq? -1 counter)
+     (set! selfValue 0)]
+    [else
+     (define selfName (list-ref outputDialogueNames counter))
+     (define selfIndex (- (length allCharactersName) (length (member selfName allCharactersName))))
+     (set! selfValue (list-ref allCharactersValues selfIndex))])
+  ;perform operations as needed
+  (cond
     [(and (eq? #f addCut) (eq? #f multCut))
-     (define numOfSelf (countKeyWords dialogue textLine))
-     (define initialValue (evaluateLine personae textLine 0))
-     (define valueAfterSelf (+ (- initialValue numOfSelf) (* numOfSelf selfValue)))
-     (if  (and (eq? 1 valueAfterSelf) (eq? 0 numOfSelf))
-          (calculateValuesWithNames textLine valueAfterSelf allCharactersName allCharactersValues)
-          valueAfterSelf)]
+     (calculateValuesArthmeticHelper textLine selfValue)]
     [(number? addCut)
-     (define valueAfterNameLeft 0)
-     (define numOfSelfLeft (countKeyWords dialogue (take textLine addCut)))
-     (define initialValueLeft (evaluateLine personae (take textLine addCut) 0))
-     (define valueAfterSelfLeft (+ (- initialValueLeft numOfSelfLeft) (* numOfSelfLeft selfValue)))
-     (cond
-       [(and (eq? 1 valueAfterSelfLeft) (eq? 0 numOfSelfLeft))
-        (set! valueAfterNameLeft (calculateValuesWithNames (take textLine addCut) valueAfterSelfLeft allCharactersName allCharactersValues))]
-       [else
-        (set! valueAfterNameLeft valueAfterSelfLeft)])
-     (define valueAfterNameRight 0)
-     (define numOfSelfRight (countKeyWords dialogue (list-tail textLine (+ 2 addCut))))
-     (define initialValueRight (evaluateLine personae (list-tail textLine (+ 2 addCut)) 0))
-     (define valueAfterSelfRight (+ (- initialValueRight numOfSelfRight) (* numOfSelfRight selfValue)))
-     (cond
-       [(and (eq? 1 valueAfterSelfRight) (eq? 0 numOfSelfRight))
-        (set! valueAfterNameRight (calculateValuesWithNames (list-tail textLine (+ 2 addCut)) valueAfterSelfRight allCharactersName allCharactersValues))]
-       [else
-        (set! valueAfterNameRight valueAfterSelfRight)])
-     ;(pretty-print textLine)
-     ;(pretty-print valueAfterNameLeft)
-     ;(pretty-print valueAfterNameRight)
-     (+ valueAfterNameLeft valueAfterNameRight)]
-    
+     (define leftValue (calculateValuesArthmeticHelper (take textLine addCut) selfValue))
+     (define rightValue (calculateValuesArthmeticHelper (list-tail textLine (+ 2 addCut)) selfValue))
+     (+ leftValue rightValue)]
     [(number? multCut)
-     (define valueAfterNameLeft 0)
-     (define numOfSelfLeft (countKeyWords dialogue (take textLine multCut)))
-     (define initialValueLeft (evaluateLine personae (take textLine multCut) 0))
-     (define valueAfterSelfLeft (+ (- initialValueLeft numOfSelfLeft) (* numOfSelfLeft selfValue)))
-     (cond
-       [(and (eq? 1 valueAfterSelfLeft) (eq? 0 numOfSelfLeft))
-        (set! valueAfterNameLeft (calculateValuesWithNames (take textLine multCut) valueAfterSelfLeft allCharactersName allCharactersValues))]
-       [else
-        (set! valueAfterNameLeft valueAfterSelfLeft)])
-     (define valueAfterNameRight 0)
-     (define numOfSelfRight (countKeyWords dialogue (list-tail textLine (+ 2 multCut))))
-     (define initialValueRight (evaluateLine personae (list-tail textLine (+ 2 multCut)) 0))
-     (define valueAfterSelfRight (+ (- initialValueRight numOfSelfRight) (* numOfSelfRight selfValue)))
-     (cond
-       [(and (eq? 1 valueAfterSelfRight) (eq? 0 numOfSelfRight))
-        (set! valueAfterNameRight (calculateValuesWithNames (list-tail textLine (+ 2 multCut)) valueAfterSelfRight allCharactersName allCharactersValues))]
-       [else
-        (set! valueAfterNameRight valueAfterSelfRight)])
-     ;(pretty-print textLine)
-     ;(pretty-print valueAfterNameLeft)
-     ;(pretty-print valueAfterNameRight)
-     (* valueAfterNameLeft valueAfterNameRight)
-     ]))
+     (define leftValue (calculateValuesArthmeticHelper (take textLine multCut) selfValue))
+     (define rightValue (calculateValuesArthmeticHelper (list-tail textLine (+ 2 multCut)) selfValue))
+     (* leftValue rightValue)]))
 
-;evaluates the values of each line
+;update dialogue values with respective function call
+(define (calculateValueFunc preFuncValue funcIndex)
+  (define funcString (list-ref settingsFuncString funcIndex))
+  (cond
+    ;function call takes 0 arguements and calls no other functions
+    [(eq? 0 (list-ref settigsFuncStatus funcIndex))
+     (calculateValuesArithmetic (string-split funcString) -1)]
+    
+    ;function call takes 1 arguements and calls no other functions
+    [(eq? 1 (list-ref settigsFuncStatus funcIndex))
+     (define leftList (take (string-split funcString) (- (length (string-split funcString)) (length (member "Hamlet" (string-split funcString))))))
+     (define rightList (rest (member "Hamlet" (string-split funcString))))
+     (calculateValuesArithmetic (append leftList (list preFuncValue) rightList) -1)]
+    
+    ;function call takes takes 1 arguements and calls another function
+    [(eq? 2 (list-ref settigsFuncStatus funcIndex))
+     (define expression (rest (member "and" (string-split funcString))))
+     (define subFuncCallList (take (string-split funcString) (- (length (string-split funcString)) (length expression))))
+     (define subFuncCall (string-join subFuncCallList " "))
+
+     (define funcIndex (- (length settingsFuncNames) (length (member subFuncCall settingsFuncNames))))
+     (define leftList (take expression (- (length expression) (length (member "Hamlet" expression)))))
+     (define rightList (rest (member "Hamlet" (string-split funcString))))
+     (define preFuncValuePrime (calculateValuesArithmetic (append leftList (list preFuncValue) rightList) -1))
+     (calculateValueFunc preFuncValuePrime funcIndex)]
+    
+    ;function call takes 0 arguements and calls another function
+    [(eq? 3 (list-ref settigsFuncStatus funcIndex))
+     (define expression (rest (member "and" (string-split funcString))))
+     (define subFuncCallList (take (string-split funcString) (- (length (string-split funcString)) (length expression))))
+     (define subFuncCall (string-join subFuncCallList " "))
+     
+     (define funcIndex (- (length settingsFuncNames) (length (member subFuncCall settingsFuncNames))))
+     (define preFuncValue (calculateValuesArithmetic expression -1))
+     (calculateValueFunc preFuncValue funcIndex)]))
+
+;differentiates and evaluates each line based on different mode of input
 (define (evaluateLine mode textLine counter)
   (cond
+    ;personae character evaluation
     [(string=? personae mode)
      (define pCounter (countKeyWords mode textLine))
      (if (> pCounter 0)
          (* (* (expt 2 pCounter) -1) (length textLine))
          (length textLine))]
-    [(string=? "settingsModeEvaluation" mode)
+    
+    ;settings type evaluation
+    [(string=? settings mode)
      (define includeHamlet (countKeyWords "settingsHamlet" textLine))
      (define includeFuncCall (countKeyWords "includeFuncCall" textLine))
      (cond
+       ;parses settings into different types based criterion specfied in 'settingsFuncStatus'
        [(and (= 0 includeHamlet) (= 0 includeFuncCall)) 0]
        [(and (= 1 includeHamlet) (= 0 includeFuncCall)) 1]
-       [(and (= 1 includeHamlet) (< 0 includeFuncCall)) 2]
-       [(and (= 0 includeHamlet) (< 0 includeFuncCall)) 3])]
-    [(string=? "settingsValueEvaluation" mode)
-     0]
+       [(and (= 1 includeHamlet) (= 1 includeFuncCall)) 2]
+       [(and (= 0 includeHamlet) (= 1 includeFuncCall)) 3])]
+    
+    ;dialogue evaluation
     [(string=? "dialogueValues" mode)
      (define includeFuncCall (countKeyWords "includeFuncCall" textLine))
-     (define tempValue (calculateValues (string-split textLine) counter))
-     tempValue
-    ]
-    [(string=? dialogue mode)
-     0]))
+     (cond
+       ;dialogue contains no function call
+       [(eq? 0 includeFuncCall)
+        (calculateValuesArithmetic (string-split textLine) counter)]
+       [else
+        (define expression (rest (member "and" (string-split textLine))))
+        (define funcCallList (take (string-split textLine) (- (length (string-split textLine)) (length expression))))
+        (define funcCall (string-join funcCallList " "))
+        (cond
+          ;dialogue contains function call that does not exist
+          [(eq? #f (member funcCall settingsFuncNames))
+           (calculateValuesArithmetic (string-split textLine) counter)]
+          ;dialogue contains a valid function call
+          [else
+           (define funcIndex (- (length settingsFuncNames) (length (member funcCall settingsFuncNames))))
+           (define preFuncValue (calculateValuesArithmetic expression counter))
+           (calculateValueFunc preFuncValue funcIndex)])])]))
 
-;evaluates each body of text
+;evaluates all text of a given mode by interpreting it line by line
 (define (evaluateText mode textList counter)
   (cond
     [(string=? personae mode)
      (if (eq? empty textList)
          '()
          (cons (evaluateLine mode (string-split (first textList)) 0) (evaluateText mode (rest textList) 0)))]
-    [(string=? "settingsModeEvaluation" mode)
+    [(string=? settings mode)
      (if (eq? empty textList)
          '()
          (cons (evaluateLine mode (first textList) 0) (evaluateText mode (rest textList) 0)))]
-    [(string=? "settingsValueEvaluation" mode)
-     (if (eq? empty textList)
-         '()
-         (cons (evaluateLine mode (string-split (first textList)) 0) (evaluateText mode (rest textList) 0)))]
     [(string=? "dialogueValues" mode)
      (if (eq? empty textList)
          '()
-         (cons (evaluateLine mode (first textList) counter) (evaluateText mode (rest textList) (+ counter 1))))]
-    [(string=? dialogue mode)
-     0]))
+         (cons (evaluateLine mode (first textList) counter) (evaluateText mode (rest textList) (+ counter 1))))]))
 
-;removes the first word from a string
-(define (removeFirstWord textList)
-  (if (eq? empty textList)
-      '()
-      (cons (string-join (rest (string-split (first textList))) " ") (removeFirstWord (rest textList)))))
-
-;retrieves the first word from a string
+;cleans the formating of a given text for storage
 (define (cleanString mode textList)
   (cond
+    [(string=? "removeFirstWord" mode)
+     (if (eq? empty textList)
+         '()
+         (cons (string-join (rest (string-split (first textList))) " ") (cleanString mode (rest textList))))]
     [(string=? personae mode)
      (if (eq? empty textList)
          '()
@@ -384,28 +392,19 @@ Read through the starter code carefully. In particular, look for:
     [(string=? settings mode)
      (if (eq? empty textList)
          '()
-         (cons (string-append "The song of " (string-trim (first (string-split (first textList))) ",") " and ") (cleanString mode (rest textList))))]))
+         (cons (string-append "The song of " (string-trim (first (string-split (first textList))) ",") " and") (cleanString mode (rest textList))))]))
 
+;evaluates entire text
 (define (evaluate body)
-  ;a list of strings that is seperated into Personae, Settings and Dialogue
   (define parsedText (bodyParser body))   
   
-  ;'allCharactersNames' and 'allCharactersValues' are two lists of names and values from Persona
   (set! allCharactersName (cleanString personae (first parsedText)))   
-  (set! allCharactersValues (evaluateText personae (removeFirstWord (first parsedText)) 0))
-    
-  ;settingsFuncNames and settingsFuncString are two lists of names and string from Settings
-  ;settigsFuncStatus represents the mode of each function where
-     ; 0 - takes 0 arguements and calls no other functions
-     ; 1 - takes 1 arguements and calls no other functions
-     ; 2 - takes 1 arguements and call at least one other function
-     ; 3 - takes 0 arguements and calls at least one other function
-  (set! settingsFuncNames (cleanString settings(second (bodyParser body))))
-  (define settingsFuncString (removeFirstWord (second parsedText)))
-  (define settigsFuncStatus (evaluateText "settingsModeEvaluation" settingsFuncString 0))
-  (define settingsFuncValues (evaluateText "settingsValueEvaluation" settingsFuncString 0))
+  (set! allCharactersValues (evaluateText personae (cleanString "removeFirstWord" (first parsedText)) 0))
   
-  ;outputDialogueNames and outputDialogueStrings are two lists of names and string from Dialogue
+  (set! settingsFuncNames (cleanString settings(second (bodyParser body))))
+  (set! settingsFuncString (cleanString "removeFirstWord" (second parsedText)))
+  (set! settigsFuncStatus (evaluateText settings settingsFuncString 0))
+
   (set! outputDialogueNames (cleanString "dialogueNames" (last parsedText)))
   (define outputDialogueStrings (cleanString "dialogueWords" (last parsedText)))
   (define outputDialogueValues (evaluateText "dialogueValues" outputDialogueStrings 0))
